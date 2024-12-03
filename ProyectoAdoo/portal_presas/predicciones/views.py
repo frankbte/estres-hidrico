fe3acec2a74a8d5f402306e4274680f202288e9a
Francisco Yanez <103383940+frankbte@users.noreply.github.com>
2024-12-02 23:50:46 -0600

    Add files via upload

---
+++ ProyectoAdoo/portal_presas/predicciones/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistroForm
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistroForm()
+    return render(request, 'registration/registro.html', {'form': form})
